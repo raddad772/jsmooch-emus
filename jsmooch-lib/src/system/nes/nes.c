36d57d9471595d905408562efb9805455e7d9a14
raddad772 <81476102+raddad772@users.noreply.github.com>
2024-10-19 17:50:23 -0400

    resampling enabled for NES

--- jsmooch-lib/src/system/nes/nes.c
+++ jsmooch-lib/src/system/nes/nes.c
@@ -188,7 +188,7 @@ static void setup_audio(struct cvec* IOs)
     struct physical_io_device *pio = cvec_push_back(IOs);
     pio->kind = HID_AUDIO_CHANNEL;
     struct JSM_AUDIO_CHANNEL *chan = &pio->audio_channel;
-    chan->sample_rate = 48000;
+    chan->sample_rate = APU_CYCLES_PER_FRAME * 60;
 }
 
 void NESJ_describe_io(JSM, struct cvec *IOs)
