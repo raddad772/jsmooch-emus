40eaeb197c938fbca7e52f7ff304366c315783b9
raddad772 <81476102+raddad772@users.noreply.github.com>
2025-03-06 20:45:36 -0500

    checkpoint

--- jsmooch-lib/src/system/ps1/timers/ps1_timers.c
+++ jsmooch-lib/src/system/ps1/timers/ps1_timers.c
@@ -178,7 +178,7 @@ static void update_irqs(struct PS1 *this)
 u32 PS1_timers_read(struct PS1 *this, u32 addr, u32 sz)
 {
     u32 timer_num = (addr >> 4) & 3;
-    switch(addr & 0x1FFFFFEF) {
+    switch(addr & 0x1FFFFFCF) {
         case 0x1F801100: // current counter value
             return read_timer(this, timer_num) & 0xFFFF;
         case 0x1F801104: { // timer0...2 mode
